8e381770fdddd4af16c0a5658d2f767f7cb179c6
Jon Abrams <jon@jonabrams.com>
2022-01-23 09:42:43 -0800

    Add bestpig patches

--- scrape.js
+++ scrape.js
@@ -18,6 +18,9 @@ const githubMdSources = {
   },
 };
 
+const bestBigSrc =
+  "https://gist.githubusercontent.com/BestPig/528fb9a19cbb638fac1278a641041881/raw/75b5236f40a9128c4de28e892bae41ebe99afc2c/analogue-pocket-game-patches.md";
+
 (async () => {
   const output = {
     updated: new Date().toISOString(),
@@ -50,6 +53,26 @@ const githubMdSources = {
       output.roms.push(rom);
     }
   }
+  // bestpig parsing
+  const text = await fetch(bestBigSrc).then((res) => res.text());
+  const chunks = text.split(/^#+\s*/m);
+  for (let chunk of chunks) {
+    const match = chunk.match(
+      /^(?<name>.*)[^]*(?<url>https:\/\/.*ips)[^]*MD5.*(?<md5>[0-9a-f]{32})/i
+    );
+    if (!match) continue;
+    const { name, md5, url } = match.groups;
+    const rom = { name, md5: md5.toLowerCase() };
+    console.log("Fetching IPS for", name);
+    const downloadUrl = url.replace(
+      "shareit.bestpig.fr/file",
+      "shareit.bestpig.fr/get"
+    );
+    const buffer = await fetch(downloadUrl).then((res) => res.arrayBuffer());
+    rom.patchIps = Base64.fromUint8Array(new Uint8Array(buffer));
+    output.roms.push(rom);
+  }
+
   output.roms.sort((a, b) => {
     if (a.name > b.name) {
       return 1;
